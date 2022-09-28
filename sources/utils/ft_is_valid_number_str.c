47dcc0c23f993dfd63c47cd53ce814e91f3e7ee6
araggohnxd <matheusnagato300@gmail.com>
2022-09-27 21:30:34 -0300

    CHORE: Remove useless cast

--- sources/utils/ft_is_valid_number_str.c
+++ sources/utils/ft_is_valid_number_str.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 10:45:25 by maolivei          #+#    #+#             */
-/*   Updated: 2022/09/05 11:36:30 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/09/27 21:28:21 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_bool	ft_is_valid_number_str(const char *str)
 {
-	const char	*start = (char *)str;
+	const char	*start = str;
 
 	if (*str == '-')
 		return (FALSE);
